77576c909425fdc20b5e0a7ef658e8e0f9e92193
AlexandraM22 <89767566+AlexandraM22@users.noreply.github.com>
2024-03-11 23:14:19 +0200

    first commit

---
+++ src/main/java/com/example/test2javafx/CartItemCell.java
@@ -0,0 +1,9 @@
+package com.example.test2javafx;
+
+import javafx.scene.control.ListCell;
+import javafx.scene.image.ImageView;
+
+public class CartItemCell extends ListCell<CartItem> {
+    private ImageView imageView = new ImageView();
+
+}
